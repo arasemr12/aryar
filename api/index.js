a2a7a008ff1435ef3cdcb4b11b1a011afbea0c2c
Aras emr <arasemr1234@gmail.com>
2022-09-02 23:00:30 +0300

    cors

--- api/index.js
+++ api/index.js
@@ -2,18 +2,25 @@ require('dotenv').config();
 require('./bot');
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin:"*"
+}))
 
 app.get('/users/:id',async(req,res) => {
     let {id} = req.params;
     if(!id) return res.json({success:false,message:"Required params!"});
 
-    let member = await client.guilds.cache.get("1013497869724033154").members.cache.get(id);
+    let member = client.guilds.cache.get("1013497869724033154").members.cache.get(id);
     if(!member) return res.json({success:false,message:"User not found!"});
     
     let activity = member.presence.activities;
     let user = member.user;
     let status = member.presence.status;
+
+    delete member.roles;
 
     return res.send({success:true,message:"Founded user!",data:{member,status,user,activity}});
 });
